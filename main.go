1acade097c27b6de1307f3333d29f4083e7fe8fb
lebronjamesuit <votrannhatminh1508@gmail.com>
2023-11-24 10:19:38 +0000

    just like Spring Boot, Go has similar interface to handle request response

--- main.go
+++ main.go
@@ -5,19 +5,42 @@ import (
 	"net/http"
 )
 
+const portNumber = ":8888"
+
+// Tach func ra rieng cho home
+
+func about(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, "This is about page")
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is home page")
+}
+
+// Tach func ra rieng cho about
+
 func main() {
 
-	fmt.Println("hello")
+	fmt.Println("inside main")
 
 	// Muc tieu la request response
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	/* http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Set("Content-Type", "json/application")
+
 		n, err := fmt.Fprintf(w, "abc strig ")
 
-		fmt.Println(n)
+		fmt.Println(fmt.Sprintf("number of byte %d", n))
 		fmt.Println(err)
 
 	})
+	*/
+	http.HandleFunc("/about", about)
 
-	http.ListenAndServe(":8888", nil)
+	http.HandleFunc("/home", home)
+
+	// start web server , if error then ignore it _
+	_ = http.ListenAndServe(portNumber, nil)
 
 }
