5c9cdb30fb1016817b98a1772159ae87a270c238
lebronjamesuit <votrannhatminh1508@gmail.com>
2023-11-28 20:29:04 +0000

    Slice in go, and template cache

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -6,7 +6,7 @@ import (
 	"text/template"
 )
 
-func RenderTemplate(w http.ResponseWriter, fileName string) {
+func RenderTemplateXoadi(w http.ResponseWriter, fileName string) {
 
 	pointerTem, _ := template.ParseFiles("./template/"+fileName, "./template/base.layout.tmpl")
 
@@ -16,4 +16,41 @@ func RenderTemplate(w http.ResponseWriter, fileName string) {
 		return
 	}
 
+}
+
+// Problem, hiting disk is very cost. we should be hitting cache
+var templateCache = make(map[string]*template.Template)
+
+func RenderTemplate(w http.ResponseWriter, fileName string) {
+
+	exitingValue := templateCache[fileName]
+	if exitingValue != nil {
+		fmt.Println("Hitting cache template")
+		err := exitingValue.Execute(w, nil)
+		if err != nil {
+			fmt.Println("Error while generating template")
+			return
+		}
+	} else {
+		pointerTemplate, err := createTemplateCache(fileName)
+		if err == nil {
+			pointerTemplate.Execute(w, nil)
+		}
+
+	}
+
+}
+
+func createTemplateCache(fileName string) (*template.Template, error) {
+	templates := []string{
+		fmt.Sprintf("./template/%s", fileName),
+		"./template/base.layout.tmpl",
+	}
+
+	pointerTemplate, err := template.ParseFiles(templates...)
+	if err != nil {
+		return nil, err
+	}
+	templateCache[fileName] = pointerTemplate
+	return pointerTemplate, nil
 }
