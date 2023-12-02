fc133e61a9a328590942fa622c8f5b450d4ac1f0
lebronjamesuit <votrannhatminh1508@gmail.com>
2023-12-02 22:49:06 +0000

    Change Algorithm of loading caches is from the start, which is improve nothing about Time Complexity

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -11,8 +11,6 @@ const portNumber = ":8888"
 
 func main() {
 
-	testcho()
-
 	http.HandleFunc("/about", handler.AboutHandler)
 
 	http.HandleFunc("/home", handler.HomeHandler)
@@ -25,42 +23,5 @@ func main() {
 
 	fmt.Print("start web server ok at localhost:8888")
 	_ = http.ListenAndServe(portNumber, nil)
-
-}
-
-func testcho() {
-	var s []string
-	fmt.Println("uninit:", s, s == nil, len(s) == 0)
-
-	s = make([]string, 3)
-	fmt.Println("emp:", s, "len:", len(s), "cap:", cap(s))
-
-	s[0] = "a"
-	s[1] = "b"
-	s[2] = "c"
-	fmt.Println("set:", s)
-	fmt.Println("get:", s[2])
-
-	fmt.Println("len:", len(s))
-
-	s = append(s, "d")
-	s = append(s, "e", "f")
-	fmt.Println("apd:", s)
-
-	c := make([]string, len(s))
-	copy(c, s)
-	fmt.Println("cpy:", c)
-
-	l := s[2:5]
-	fmt.Println("sl1:", l)
-
-	l = s[:5]
-	fmt.Println("sl2:", l)
-
-	l = s[2:]
-	fmt.Println("sl3:", l)
-
-	t := []string{"g", "h", "i"}
-	fmt.Println("dcl:", t)
 
 }
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,56 +1,97 @@
 package render
 
 import (
-	"fmt"
+	"log"
 	"net/http"
+	"path/filepath"
 	"text/template"
 )
 
-func RenderTemplateXoadi(w http.ResponseWriter, fileName string) {
-
-	pointerTem, _ := template.ParseFiles("./template/"+fileName, "./template/base.layout.tmpl")
-
-	err := pointerTem.Execute(w, nil)
+// Runing now
+func RenderTemplate(w http.ResponseWriter, fileName string) {
+	mapResult, err := createAllTemplate()
 	if err != nil {
-		fmt.Println("Error generating template")
-		return
+		log.Fatal("Template not found")
 	}
+	currentTemp := mapResult[fileName]
+	currentTemp.Execute(w, nil)
 
 }
 
-// Problem, hiting disk is very cost. we should be hitting cache
-var templateCache = make(map[string]*template.Template)
+// create a new tempalte that load all files in folder Template at once.
+func createAllTemplate() (map[string]*template.Template, error) {
 
-func RenderTemplate(w http.ResponseWriter, fileName string) {
+	myCaches := make(map[string]*template.Template)
 
-	exitingValue := templateCache[fileName]
-	if exitingValue != nil {
-		fmt.Println("Hitting cache template")
-		err := exitingValue.Execute(w, nil)
+	sliceOfPathFilesName, err := filepath.Glob("./template/*.page.tmpl")
+	if err != nil {
+		log.Fatal("Error when load template", err)
+		return nil, err
+	}
+
+	for _, path := range sliceOfPathFilesName {
+		fileName := filepath.Base(path)
+		ts, err := template.ParseFiles(path)
 		if err != nil {
-			fmt.Println("Error while generating template")
+			log.Fatal("error creating Template")
+		}
+		layout := "./template/*.layout.tmpl"
+		matches, err1 := filepath.Glob(layout)
+		if err1 == nil && len(matches) > 0 {
+			ts, _ = ts.ParseGlob(layout) // Attach layout to page
+		}
+
+		myCaches[fileName] = ts // template
+	}
+
+	// return the map
+	return myCaches, nil
+
+}
+
+//var templateCache = make(map[string]*template.Template)
+
+/*
+// Render Template
+func RenderTemplate_Pause(w http.ResponseWriter, fileName string) {
+	existing := templateCache[fileName]
+	if existing != nil {
+		log.Println("existing  template of tc")
+		err := existing.Execute(w, nil)
+		if err != nil {
+			fmt.Println("Error generating existing")
 			return
 		}
 	} else {
-		pointerTemplate, err := createTemplateCache(fileName)
-		if err == nil {
-			pointerTemplate.Execute(w, nil)
+		log.Println("create new template for tc")
+		pTemplate := createNewTemplate(fileName)
+		log.Println("after creating a new template for tc")
+
+		err := pTemplate.Execute(w, nil)
+		if err != nil {
+			fmt.Println("Error generating pTemplate")
+			return
 		}
-
 	}
-
 }
 
-func createTemplateCache(fileName string) (*template.Template, error) {
-	templates := []string{
+// create a new template and store it in map cache
+func createNewTemplate(fileName string) *template.Template {
+
+	sliceFiles := []string{
 		fmt.Sprintf("./template/%s", fileName),
 		"./template/base.layout.tmpl",
 	}
 
-	pointerTemplate, err := template.ParseFiles(templates...)
+	pTemplate, err := template.ParseFiles(sliceFiles...)
 	if err != nil {
-		return nil, err
+		log.Println("create new template error")
+		return nil
+	} else {
+		log.Println("add new  template to caches and return")
+		templateCache[fileName] = pTemplate
+		return pTemplate
 	}
-	templateCache[fileName] = pointerTemplate
-	return pointerTemplate, nil
 }
+
+*/
